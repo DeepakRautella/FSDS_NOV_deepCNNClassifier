15f330c04573267dd2d3508dfdac28beb86264fa
DeepakRautella <deepak.rautella@gmail.com>
2022-09-19 16:27:00 +0530

    basic_updated

--- src/deepClassifier/utils/common.py
+++ src/deepClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from deepClassifier import logger
+from deepClassifier import *
 import json
 import joblib
 from ensure import ensure_annotations
