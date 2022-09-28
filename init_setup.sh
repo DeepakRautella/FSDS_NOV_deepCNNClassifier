d021e31c541790732150a94ba30795cf6f38322c
DeepakRautella <deepak.rautella@gmail.com>
2022-09-28 16:59:34 +0530

    Revert "python_version_updated"
    This reverts commit 1a5ad4c89c9f9e84a59da787088c0b8548137f29.

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START" 
-echo [$(date)]: "creating env with python 3.8.13 version" 
-conda create --prefix ./env python=3.8.13 -y
+echo [$(date)]: "creating env with python 3.8 version" 
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activating the environment" 
 source activate ./env
 echo [$(date)]: "installing the dev requirements" 
