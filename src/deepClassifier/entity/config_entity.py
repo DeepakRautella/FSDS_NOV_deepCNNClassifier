6ef7a61e84bac3982f8f345aab12ea25dd0de3e3
DeepakRautella <deepak.rautella@gmail.com>
2022-09-28 17:26:28 +0530

    entity_updated

--- src/deepClassifier/entity/config_entity.py
+++ src/deepClassifier/entity/config_entity.py
@@ -8,8 +8,9 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
-dataclass(frozen=True)
 
+
+@dataclass(frozen=True)
 class PrepareBaseModelConfig:
     root_dir: Path
     base_model_path: Path
@@ -25,3 +26,22 @@ class PrepareCallbacksConfig:
     root_dir: Path
     tensorboard_root_log_dir: Path
     checkpoint_model_filepath: Path
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    params_image_size: list
+    params_batch_size: int
